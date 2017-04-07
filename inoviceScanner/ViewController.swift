500006b5158b513a1fdfaeb3f76efc9857f8a290
黃聖傑 <justinhuang@huangshengjiede-iMac.local>
2017-04-07 14:20:49 +0800

    Initial Commit

---
+++ inoviceScanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  inoviceScanner
+//
+//  Created by 黃聖傑 on 2017/4/7.
+//  Copyright © 2017年 findata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
