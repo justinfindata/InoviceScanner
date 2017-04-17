72d4eb670ec9ff41433e83ede06fbd4cb1c8e4af
黃聖傑 <justinhuang@huangshengjiede-iMac.local>
2017-04-17 09:02:16 +0800

    first commit

---
+++ inoviceScanner/InoviceModel.swift
@@ -0,0 +1,9 @@
+//
+//  InoviceModel.swift
+//  inoviceScanner
+//
+//  Created by 黃聖傑 on 2017/4/10.
+//  Copyright © 2017年 findata. All rights reserved.
+//
+
+import Foundation
---
+++ inoviceScanner/InoviceScannerNative.swift
@@ -0,0 +1,9 @@
+//
+//  InoviceScannerNative.swift
+//  inoviceScanner
+//
+//  Created by 黃聖傑 on 2017/4/11.
+//  Copyright © 2017年 findata. All rights reserved.
+//
+
+import Foundation
